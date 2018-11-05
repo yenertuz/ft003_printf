efc3b4f87067cce1545fdb3c997bb2508af9ca90
Yener Tüz <yenertuz@Yeners-iMac.local>
2018-11-05 00:10:06 -0800

    modified printf_apply_flags.c

--- libft/src/printf_apply_flags.c
+++ libft/src/printf_apply_flags.c
@@ -1,6 +1,25 @@
 #include "libft.h"
 
-void	printf_apply_flags(void)
+void	printf_apply_flags(char **output, t_printf *data)
 {
-	;
+	if (printf_is_signed(data) == 1 && **output != '-')
+	{
+		if (ft_strchr(data->flags, '+') != 0)
+		{
+			ft_prepend(output, "+");
+			return ;
+		}
+		if (ft_strchr(data->flags, " ") != 0)
+			ft_prepend(output, " ");
+	}
+	if (printf_is_unsigned(data) == 1 && ft_strchr(data->flags, "#") != 0 &&
+		is_zero(*output) == 0)
+	{
+		if (data->specifier == 'o')
+			ft_prepend(output, "0");
+		else if (data->specifier == "x")
+			ft_prepend(output, "0x");
+		else if (data->specifier == "X")
+			ft_prepend(output, "0X");
+	}
 }
